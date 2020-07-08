c598796874db8421cdda97e984c9ccca135ca59a
Diego Sanchez <difa1124@hotmail.com>
2020-07-08 17:07:23 -0500

    Cargando Data, plots y volcano

---
+++ Plotandvolcano.R
@@ -0,0 +1,20 @@
+library(plotly)
+## Loading required package: ggplot2
+## 
+## Attaching package: 'plotly'
+## The following object is masked from 'package:ggplot2':
+## 
+##     last_plot
+## The following object is masked from 'package:stats':
+## 
+##     filter
+## The following object is masked from 'package:graphics':
+## 
+##     layout
+
+plot_ly(z=volcano, type="surface")
+
+## Boxplot
+library(plotly)
+p <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
+p
